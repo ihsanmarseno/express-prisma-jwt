9095799df1553628b67245e96dae707da979f1bf
Ihsan Tri Marseno <ihsanmarseno09@gmail.com>
2024-03-03 16:57:19 +0700

    add swagger ui

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,22 @@ import authRouter from "./routes/auth";
 import tasksRouter from "./routes/tasks";
 import validationToken from "./utils/validationToken";
 import { updateTaskStatus } from "./utils/cronJobs";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "./swagger.json";
 
 const app = express();
 const PORT = 5000;
 
-app.use(express.json());
 updateTaskStatus();
 
-app.use("/auth", authRouter);
-app.use("/tasks", validationToken, tasksRouter);
+app.use(express.json());
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.use("/auth", authRouter);
+app.use("/tasks", validationToken, tasksRouter); 
+
+// Menjalankan server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
