4d298cc00854172754a62168d655a554e68e193a
Ihsan Tri Marseno <ihsanmarseno09@gmail.com>
2024-03-03 17:44:32 +0700

    add css url for swagger

--- src/index.ts
+++ src/index.ts
@@ -8,15 +8,21 @@ import swaggerDocument from "./swagger.json";
 
 const app = express();
 const PORT = 5000;
+const CSS_URL =
+  "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
 
 updateTaskStatus();
 
 app.use(express.json());
 
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(
+  "/docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument, { customCssUrl: CSS_URL })
+);
 
 app.use("/auth", authRouter);
-app.use("/tasks", validationToken, tasksRouter); 
+app.use("/tasks", validationToken, tasksRouter);
 
 // Menjalankan server
 app.listen(PORT, () => {
