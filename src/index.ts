ff722199269945edc5a55835ff5363489f0644ee
Ihsan Tri Marseno <ihsanmarseno09@gmail.com>
2024-03-03 18:20:07 +0700

    add css url

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,11 @@ app.use(express.json());
 app.use(
   "/docs",
   swaggerUi.serve,
-  swaggerUi.setup(swaggerDocument, { customCssUrl: CSS_URL })
+  swaggerUi.setup(swaggerDocument, {
+    customCss:
+      ".swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }",
+    customCssUrl: CSS_URL,
+  })
 );
 
 app.use("/auth", authRouter);
