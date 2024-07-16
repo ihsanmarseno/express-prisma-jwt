199b37c837d5f21b1be8557dd5b200703016dbf8
Ihsan Tri Marseno <ihsanmarseno09@gmail.com>
2024-07-16 23:48:17 +0700

    getTaskById on routes

--- src/routes/tasks.ts
+++ src/routes/tasks.ts
@@ -2,6 +2,7 @@ import express from "express";
 import {
   createTask,
   deleteTask,
+  getTaskById,
   getTasks,
   updateStatusById,
   updateTaskById,
@@ -11,7 +12,7 @@ const router = express.Router();
 
 router.post("/create", createTask);
 router.get("/", getTasks);
-router.put("/task/:id");
+router.put("/task/:id", getTaskById);
 router.delete("/delete/:id", deleteTask);
 router.put("/update/:id", updateTaskById);
 router.put("/update-status/:id", updateStatusById);
